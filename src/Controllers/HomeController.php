cafedba67b8db1066a921842cf0b0ee68d776c8d
Bobby Hines <bobby@conflabs.com>
2023-11-17 20:38:14 -0800

    Some basic error handling added.
    Signed-off-by: Bobby Hines <bobby@conflabs.com>

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -15,10 +15,34 @@ final class HomeController extends Controller
             echo $this->renderView('Home.twig', []);
         } else {
 
+            $urlIsGoogleDrive = str_contains($url, 'https://drive.google.com/file/d/');
+            if (!$urlIsGoogleDrive) {
+
+                header("HTTP/1.1 400 OK");
+                header('Content-Type: application/json');;
+                echo json_encode([
+                    'status' => 400,
+                    'message' => 'Invalid URL in request. Must be a Google Drive URL.',
+                ]);
+                return;
+            }
+
             $fileId = preg_replace('/\/view.*/', '', str_replace('https://drive.google.com/file/d/', '', $url));
 
+            $buffer = file_get_contents('https://drive.google.com/uc?export=download&id=' . $fileId);
+            if (!$buffer) {
+                header("HTTP/1.1 400 OK");
+                header('Content-Type: application/json');;
+                echo json_encode([
+                    'status' => 400,
+                    'message' => 'Content empty. No data found for this URL.',
+                ]);
+                return;
+            }
+
+            header("HTTP/1.1 200 OK");
             header('Content-Type: application/json');
-            echo file_get_contents('https://drive.google.com/uc?export=download&id=' . $fileId);
+            echo $buffer;
         }
     }
 }
