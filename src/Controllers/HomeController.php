8fd491e0feee65ef82caae5c9a2962c725055e2a
Bobby Hines <bobby@conflabs.com>
2023-11-19 12:02:53 -0800

    Methods broken up for readability. Comps added to handle an ID parameter.
    Signed-off-by: Bobby Hines <bobby@conflabs.com>

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace Conflabs\JsonFileViewer\Controllers;
 
 
-
 use Symfony\Component\HttpFoundation\Response;
 
 final class HomeController extends Controller
@@ -19,57 +18,144 @@ final class HomeController extends Controller
         // Get the url link from the request, if it exists.
         $url = $_GET['url'] ?? null;
 
-        // If the url link isn't in the request...
+        // If it does exist...
+        if ($url) {
+
+            // ...return the url parameter method.
+            return $this->urlParameter($url);
+        }
+
+        // Get the id from the request, if it exists.
+        $id = $_GET['id'] ?? null;
+
+        // If it does exist...
+        if ($id) {
+
+            // ...return the id parameter method.
+            return $this->idParameter($id);
+        }
+
+        // If the url and id aren't in the request, return the no parameter method.
+        return $this->noParameter();
+    }
+
+    /**
+     * @return Response
+     */
+    protected function noParameter(): Response
+    {
+
+        // ...return the default home page.
+        $response = new Response($this->renderView('Home.twig', ['year' => date('Y')]), Response::HTTP_OK, ['content-type' => 'text/html']);
+        return $response->send();
+    }
+
+    /**
+     * @param string|null $url
+     * @return Response
+     */
+    protected function urlParameter(string $url = null): Response
+    {
+
+        // If there's no URL in the request...
         if (!$url) {
 
-            // ...return the default home page.
-            $response = new Response($this->renderView('Home.twig', ['year' => date('Y')]), Response::HTTP_OK, ['content-type' => 'text/html']);
+            // ...Form a 400 Bad Request error with useful message...
+            $response = new Response(json_encode([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => 'No URL found in request.'
+            ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
 
+            // ...and return it.
             return $response->send();
 
-            // Otherwise, if the url link is in the request...
-        } else {
-
-            // Check to see that it's a Google Drive URL.
-            $urlIsGoogleDrive = str_contains($url, 'https://drive.google.com/file/d/');
-
-            // If it's not a Google Drive URL...
-            if (!$urlIsGoogleDrive) {
-
-                // ...return a 400 Bad Request error with useful message.
-                $response = new Response(json_encode([
-                    'status' => Response::HTTP_BAD_REQUEST,
-                    'message' => 'Invalid URL in request. Must be a Google Drive URL.',
-                ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
-
-                return $response->send();
-
-                // The rest of the code in this method will only run if the URL is a Google Drive URL.
-            }
-
-            // Remove everything but the file ID from the URL.
-            $fileId = preg_replace('/\/view.*/', '', str_replace('https://drive.google.com/file/d/', '', $url));
-
-            // Get the file contents from Google Drive link and store in buffer.
-            $buffer = file_get_contents('https://drive.google.com/uc?export=download&id=' . $fileId);
-
-            // If the buffer is empty...
-            if (!$buffer) {
-
-                // ...return a 400 Bad Request error with useful message.
-                $response = new Response(json_encode([
-                    'status' => Response::HTTP_BAD_REQUEST,
-                    'message' => 'No data found for this URL.'
-                ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
-
-                return $response->send();
-
-                // The rest of the code in this method will only run if the buffer is not empty.
-            }
-
-            // Return the buffer as a JSON response.
-            $response = new Response($buffer, Response::HTTP_OK, ['content-type' => 'application/json']);
-            return $response->send();
+            // The rest of the code in this method will only run if there is a URL in the request.
         }
+
+        // Check to see that it's a Google Drive URL.
+        $urlIsGoogleDrive = str_contains($url, 'https://drive.google.com/file/d/');
+
+        // If it's not a Google Drive URL...
+        if (!$urlIsGoogleDrive) {
+
+            // ...return a 400 Bad Request error with useful message.
+            $response = new Response(json_encode([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => 'Invalid URL in request. Must be a Google Drive URL.',
+            ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
+
+            return $response->send();
+
+            // The rest of the code in this method will only run if the URL is a Google Drive URL.
+        }
+
+        // Remove everything but the file ID from the URL.
+        $fileId = preg_replace('/\/view.*/', '', str_replace('https://drive.google.com/file/d/', '', $url));
+
+        // Get the file contents from Google Drive link and store in buffer.
+        $buffer = file_get_contents('https://drive.google.com/uc?export=download&id=' . $fileId);
+
+        // If the buffer is empty...
+        if (!$buffer) {
+
+            // ...return a 400 Bad Request error with useful message.
+            $response = new Response(json_encode([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => 'No data found for this URL.'
+            ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
+
+            return $response->send();
+
+            // The rest of the code in this method will only run if the buffer is not empty.
+        }
+
+        // Return the buffer as a JSON response.
+        $response = new Response($buffer, Response::HTTP_OK, ['content-type' => 'application/json']);
+
+        return $response->send();
+    }
+
+    /**
+     * @param string|null $id
+     * @return Response
+     */
+    protected function idParameter(string $id = null): Response
+    {
+
+        // If there's no ID in the request...
+        if (!$id) {
+
+            // ...Form a 400 Bad Request error with useful message...
+            $response = new Response(json_encode([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => 'No ID found in request.'
+            ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
+
+            // ...and return it.
+            return $response->send();
+
+            // The rest of the code in this method will only run if there is an ID in the request.
+        }
+
+        // Get the file contents from Google Drive link and store in buffer.
+        $buffer = file_get_contents('https://drive.google.com/uc?export=download&id=' . $id);
+
+        // If the buffer is empty...
+        if (!$buffer) {
+
+            // ...return a 400 Bad Request error with useful message.
+            $response = new Response(json_encode([
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => 'No data found for this URL.'
+            ]), Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
+
+            return $response->send();
+
+            // The rest of the code in this method will only run if the buffer is not empty.
+        }
+
+        // Return the buffer as a JSON response.
+        $response = new Response($buffer, Response::HTTP_OK, ['content-type' => 'application/json']);
+        return $response->send();
     }
 }
